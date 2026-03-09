21f9168fb4677528c17bcc29285e25b0d42d7281
gangjeuk <65859590+gangjeuk@users.noreply.github.com>
2026-03-09 17:13:24 +0900

    feat(sdk): newton adapter can handle multiple runners

--- tt/sdk/runner_service/local/newton_adapter.py
+++ tt/sdk/runner_service/local/newton_adapter.py
@@ -17,8 +17,6 @@ if TYPE_CHECKING:
         IEnvironmentManagementService,
     )
     from tt.sdk.log_service.common.log_service import ILogService
-    import newton
-    import warp as wp
 
 
 class NewtonRunner(IRunner):
@@ -46,7 +44,6 @@ class NewtonRunner(IRunner):
     def reset(self) -> None: ...
     def set_state(self) -> None: ...
 
-class NewtonAdapter(IPhysicsEngineAdapter):
 
 class NewtonAdapter(IPhysicsEngineAdapter):
     def __init__(
@@ -67,6 +64,8 @@ class NewtonAdapter(IPhysicsEngineAdapter):
         self._step_count_map: MutableMapping[str, int] = dict()
 
         self.builder = newton.ModelBuilder()
+        self.__scene = newton.ModelBuilder()
+
         self.model: newton.Model | None = None
 
         self._solver: newton.solvers.SolverBase | None = None
@@ -74,8 +73,7 @@ class NewtonAdapter(IPhysicsEngineAdapter):
         self._state_1: newton.State | None = None
         self._control: newton.Control | None = None
         self._contacts: newton.Contacts | None = None
-
-        self._runner_count = 0
+        self._viewer: newton.viewer.ViewerGL = newton.viewer.ViewerGL()
 
         env_ret = self.EnvironmentManagementService.get_environment(self.env_id)
 
@@ -86,32 +84,41 @@ class NewtonAdapter(IPhysicsEngineAdapter):
 
         self.env = env
 
-    def initialize(self):
-
         objects = self.env.objects
 
         for obj in objects:
             print(obj.physics.mjcf_uri)
 
-            # if "franka" in obj.physics.mjcf_uri:
-            #     self.builder.add_urdf(obj.physics.mjcf_uri)
-            # else:
-            # TODO: URDF file is not handled here. Need handling code
-            self.__scene.add_mjcf(obj.physics.mjcf_uri, xform=(obj.pos + obj.quat))
+            if "franka" in obj.physics.mjcf_uri:
+                self.__scene.add_urdf(obj.physics.mjcf_uri)
+            else:
+                # TODO: URDF file is not handled here. Need handling code
+                self.__scene.add_mjcf(obj.physics.mjcf_uri, xform=(obj.pos + obj.quat))
 
     def initialize(self) -> None: ...
 
     def create_runner(self) -> IRunner:
+        if self._step_count != 0:
+            raise TektonianBaseError(
+                "Cannot create new runner after calling step() function"
+            )
 
-        def runner_step(world_idx: int, action: list[float]):
+        def runner_step(runner_idx: int, action: list[float]):
             if self.model is None:
-                self.builder.replicate(self._runner_count)
+                self.builder.replicate(self.__scene, world_count=self._runner_count)
+
+                # TODO: Remove later
+                self.__set_debug_env(self._runner_count)
+
                 self.model = self.builder.finalize()
-                self._solver = self.newton.solvers.SolverMuJoCo(self.model)
+                self._solver = newton.solvers.SolverMuJoCo(self.model)
                 self._state_0 = self.model.state()
                 self._state_1 = self.model.state()
                 self._control = self.model.control()
                 self._contacts = self.model.contacts()
+
+                # TODO: Remove later
+                self.__set_debug_viewer(0)
 
             if self.model.max_dofs_per_articulation != len(action):
                 self.LogService.warn(
@@ -129,13 +136,13 @@ class NewtonAdapter(IPhysicsEngineAdapter):
                 assert False, "unreachable"
 
             # Compute segment for this world instance
-            s1 = self.model.max_dofs_per_articulation * world_idx
+            s1 = self.model.max_dofs_per_articulation * runner_idx
             s2 = s1 + self.model.max_dofs_per_articulation
 
             # Copy action into the segment using wp.copy to avoid slice assignment
             src: wp.array[wp.float32] = wp.array(action, dtype=wp.float32)
             count = s2 - s1  # == self.model.max_dofs_per_articulation
-            self.warp.copy(
+            wp.copy(
                 self._control.joint_target_pos,
                 src,
                 dest_offset=s1,
@@ -143,13 +150,21 @@ class NewtonAdapter(IPhysicsEngineAdapter):
                 count=count,
             )
 
-            self._solver.step(
-                self._state_0,
-                self._state_1,
-                self._control,
-                self._contacts,
-                1.0 / 60.0 / 4.0,  # TODO change with tick
-            )
+            for _ in range(10):
+                self._state_0.clear_forces()
+                self._solver.step(
+                    self._state_0,
+                    self._state_1,
+                    self._control,
+                    self._contacts,
+                    1.0 / 60.0 / 4.0,  # TODO change with tick
+                )
+                self._state_0, self._state_1 = self._state_1, self._state_0
+            self._step_count += 1
+            self._step_count_map[f"{IRunner.__ID_PREVIX}{runner_idx}"] += 1
+
+            # TODO: Remove later
+            self.__set_debug_viewer((1.0 / 60.0 / 4.0) * self._step_count)
 
         runner = NewtonRunner(
             env_id=self.env.id, runner_idx=self._runner_count, step=runner_step
@@ -168,3 +183,38 @@ class NewtonAdapter(IPhysicsEngineAdapter):
         return IPhysicsEngineAdapterState(
             self.env_id, self._runner_count, self._step_count_map
         )
+
+    def __set_debug_env(self, count: int) -> None:
+        """Debug purpose for now, handle 'fr3_franka_hand.urdf' asset setting"""
+        init_q = [
+            -3.6802115e-03,
+            2.3901723e-02,
+            3.6804110e-03,
+            -2.3683236e00,
+            -1.2918962e-04,
+            2.3922248e00,
+            7.8549200e-01,
+        ]
+        for idx in range(count):
+            s_b_9, s_a_9 = idx * 9, (idx + 1) * 9
+            self.builder.joint_q[s_b_9:s_a_9] = [*init_q, 0.05, 0.05]
+            self.builder.joint_target_pos[s_b_9:s_a_9] = [*init_q, 1.0, 1.0]
+
+            """
+            Newton MUST set this value manually for mjcf -> Think MJCF file parser error
+            """
+            self.builder.joint_target_ke[s_b_9:s_a_9] = [650.0] * 9
+            self.builder.joint_target_kd[s_b_9:s_a_9] = [100.0] * 9
+
+            self.builder.joint_effort_limit[s_b_9 : s_b_9 + 7] = [80.0] * 7
+            self.builder.joint_effort_limit[s_b_9 + 7 : s_a_9] = [20.0] * 2
+            self.builder.joint_armature[s_b_9 : s_b_9 + 7] = [0.1] * 7
+            self.builder.joint_armature[s_b_9 + 7 : s_a_9] = [0.5] * 2
+
+    def __set_debug_viewer(self, time: float):
+        if self._step_count == 0:
+            self._viewer.set_model(self.model)
+
+        self._viewer.begin_frame(time)
+        self._viewer.log_state(self._state_0)
+        self._viewer.end_frame()
