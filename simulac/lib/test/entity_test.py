40bf20b3caa71133131f4496139fe7a7b15c8857
gangjeuk <65859590+gangjeuk@users.noreply.github.com>
2026-03-29 10:39:53 +0900

    fix(test): fixed wrong stuff path and applied changed function name

--- simulac/lib/test/entity_test.py
+++ simulac/lib/test/entity_test.py
@@ -3,10 +3,10 @@ from simulac.lib.world_maker.object import Environment, Runner, Stuff
 
 def test_entity_build():
     env = Environment()
-    basket = Stuff("simulac/trash/asset/test/Basket/Basket026/model.xml")
+    basket = Stuff("trash/assets/asset/test/Basket/Basket026/model.xml")
 
-    basket_obj1 = env.place_stuff_entity(basket, (1, 0, 0))
-    basket_obj2 = env.place_stuff_entity(basket, (0, 1, 0))
+    basket_obj1 = env.place_entity(basket, (1, 0, 0))
+    basket_obj2 = env.place_entity(basket, (0, 1, 0))
 
     runner = Runner(env)
 
